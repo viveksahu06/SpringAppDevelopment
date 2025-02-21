9bc8aec01c94bae1503639116cf72dc3151eef53
viveksahu06 <viveksahu1006@gmail.com>
2025-02-21 15:10:57 +0530

    [Vivek] Add. Spring hello application

--- src/main/java/com/spring/appdevelopment/controller/Controller.java
+++ src/main/java/com/spring/appdevelopment/controller/Controller.java
@@ -7,10 +7,7 @@
 //- A quick way to jumpstart ones learning
 package com.spring.appdevelopment.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -29,6 +26,11 @@ public class Controller {
     @ResponseBody
     public String hello() {
         return "Hello, welcome to Spring Boot!";
+    }
+
+    @PutMapping("/hello")
+    public String putHello(){
+        return "Hello from BridgeLabz";
     }
 
 
